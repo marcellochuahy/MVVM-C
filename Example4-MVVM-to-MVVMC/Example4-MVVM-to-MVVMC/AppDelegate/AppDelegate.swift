0c656e40c82e72785437482eda94821729611a80
marcellochuahy <marcellochuahy@icloud.com>
2021-09-03 11:16:43 -0300

    Fixes bugs

---
+++ Example4-MVVM-to-MVVMC/Example4-MVVM-to-MVVMC/AppDelegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Example4-MVC-ViewCode
+//
+//  Created by Marcello Chuahy on 16/02/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+  
+}
+
---
+++ Example4-MVVM-to-MVVMC/Example4-MVVM-to-MVVMC/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Example2-MVC-ViewCode
+//
+//  Created by Marcello Chuahy on 16/02/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = TabBarController()
+    window?.makeKeyAndVisible()
+    
+  }
+  
+}
